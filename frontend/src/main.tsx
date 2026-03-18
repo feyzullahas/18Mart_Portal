dd20bb3c5e543e6cc5d8f15e03460dd4525eff8c
Feyzullah As <feyzullahas47@gmail.com>
2026-03-19 02:16:59 +0300

    analiz için paket yüklendi

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import { pdfjs } from 'react-pdf'
 import workerSrc from 'pdfjs-dist/build/pdf.worker.min.mjs?url'
 import './index.css'
@@ -10,5 +11,6 @@ pdfjs.GlobalWorkerOptions.workerSrc = workerSrc;
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
